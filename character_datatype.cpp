4ed55e3412ee5ce2b0f7cf3b4d75ec17f2da1f5a
Prajwal Raman KS <130436749+Ramanthecoder@users.noreply.github.com>
2023-10-03 10:52:14 +0530

    Create character_datatype.cpp

---
+++ character_datatype.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main(){
+//to declare character
+     char ch;
+cin >> ch;
+     cout << ch;
+     return 0;
+     
+}
+
+//to assign a character
+int main()
+char ch = 'p';
